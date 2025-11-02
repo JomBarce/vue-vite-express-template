3c843e93b5c46270944af805a2bc92f44d4de63f
JomBarce <89145244+JomBarce@users.noreply.github.com>
2025-11-02 23:13:37 +0800

    Updated config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+
+const BACKEND_PORT = Number(process.env.PORT) || 9001;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 9000,
+    proxy: {
+      '/api': {
+        target: `http://localhost:${BACKEND_PORT}`,
+        changeOrigin: true,
+      },
+    },
+  },
 });
