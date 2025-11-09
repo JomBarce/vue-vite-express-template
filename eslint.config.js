7123078f688e5478983f93ca0a9361a3ad5261d1
JomBarce <89145244+JomBarce@users.noreply.github.com>
2025-11-09 19:38:15 +0800

    Updated eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,26 +2,32 @@ import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
-import css from '@eslint/css';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
+  // JavaScript + TypeScript files
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
   },
+
+  // TypeScript rules
   tseslint.configs.recommended,
+
+  // Vue rules
   pluginVue.configs['flat/essential'],
+
+  // Vue files
   {
     files: ['**/*.vue'],
-    languageOptions: { parserOptions: { parser: tseslint.parser } },
-  },
-  {
-    files: ['**/*.css'],
-    plugins: { css },
-    language: 'css/css',
-    extends: ['css/recommended'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
   },
 ]);
